1869b8b138b2bf7c2b674b92f39e3e9b97b5dadc
Ajit Kumar G <ajit.gunturi@gmail.com>
2015-08-06 16:52:03 +0530

    Update falseTest.js

--- test/falseTest.js
+++ test/falseTest.js
@@ -2,6 +2,6 @@ var expect = require('chai').expect;
 
 describe("A basic test", function(){
   it('should pass when everything is not okay', function(){
-    expect(false).to.be.false;
+    expect(true).to.be.false;
   });
 });
